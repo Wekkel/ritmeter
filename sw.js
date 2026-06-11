ef077ea4cd2e53e39f3da6014564c1ccd40dde3e
Wekkel <wekkel_e@hotmail.com>
2026-06-11 19:34:04 +0200

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - overige eigen bestanden: cache-first voor snelle start
    - kaarttegels en fonts: altijd via het netwerk */
 
-const CACHE = "ritmeter-v3";
+const CACHE = "ritmeter-v4";
 const SHELL = [
   "./",
   "./index.html",
