e6786e3f7f19585c5e405290d59822382a91a1ff
Wekkel <wekkel_e@hotmail.com>
2026-06-11 22:31:14 +0200

    Update cache version from v5 to v6

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - overige eigen bestanden: cache-first voor snelle start
    - kaarttegels en fonts: altijd via het netwerk */
 
-const CACHE = "ritmeter-v5";
+const CACHE = "ritmeter-v6";
 const SHELL = [
   "./",
   "./index.html",
