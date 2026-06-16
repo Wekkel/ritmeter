fac0f903c8b335fe6d06a807b3577e65d1dacfe7
Wekkel <wekkel_e@hotmail.com>
2026-06-16 16:44:20 +0200

    Update cache version from v9 to v10
    upgrade to v10 because of solving navigation issues + horizontal mode check

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - overige eigen bestanden: cache-first voor snelle start
    - kaarttegels en fonts: altijd via het netwerk */
 
-const CACHE = "ritmeter-v9";
+const CACHE = "ritmeter-v10";
 const SHELL = [
   "./",
   "./index.html",
